580278f61351d47da521e5eeef8fa6fa6c2d399c
Boxiang Liu <jollier.liu@gmail.com>
2021-11-02 10:02:17 -0700

    modified:   cai_sim_anneal/aggregate.py 	modified:   pipe.sh

--- cai_sim_anneal/aggregate.py
+++ cai_sim_anneal/aggregate.py
@@ -1,25 +1,47 @@
-import pickle as pkl
+import dill
 from cai_sim_anneal.optimize import Plotter
+import pandas as pd
+import os
+from utils.utils import cfg_file, load_config
+import argparse 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", type=str, help="input pickle files")
+parser.add_argument("--fig", type=str, help="output figure file")
+args = parser.parse_args()
+
+# setup:
+cfg = load_config(cfg_file)
+ref_p_file = cfg.DATA.RAW.REF_P.HUMAN
 ref_points = pd.read_csv(ref_p_file)
 plotter = Plotter(ref_points)
-
 color = ["orange", "green", "red", "purple", "brown", "pink", "gray", "olive", "cyan", "magenta", "black", "yellow"]
 
-for i, lambda_ in enumerate([0.5, 1, 2, 3, 4, 6, 8, 10]):
-    pkl_file = os.path.join(cfg.DATA.PROCESSED.CAI_ANNEAL, f"results_it-30000_factor-0.001_lamb-{lambda_}_linfold_onlyHigherCAI.pkl")
+
+def extract_lambda_from_filename(filename):
+    split_filename = filename.strip().split("_")
+    for i in split_filename:
+        if i.startswith("lamb"):
+            return float(i.replace("lamb-", ""))
+
+# load different lambdas:
+
+for i, pkl_file in enumerate(args.input.split(" ")):
+    # pkl_file = os.path.join(cfg.DATA.PROCESSED.CAI_ANNEAL, f"results_it-30000_factor-0.001_lamb-{lambda_}_linfold_onlyHigherCAI.pkl")
     with open(pkl_file, "rb") as f:
-        results = pkl.load(f)["results"]
+        results = dill.load(f)["results"]
 
     sim_anneal = [(k, v["MFE"], v["CAI"]) for k, v in results.items()]
     sim_anneal = pd.DataFrame(
         sim_anneal, columns=["Iteration", "MFE", "CAI"])
 
-    plotter.add_points(sim_anneal, name="Simulated Annealing",
+    lambda_ = extract_lambda_from_file(pkl_file)
+    plotter.add_points(sim_anneal, name=f"lambda: {lambda_}",
                        color=color[i], shape="^")
 
 
-# make plot
+# make plot:
 p = plotter.plot_cai_vs_mfe()
 fig_file = os.path.join(
-    cfg.DATA.PROCESSED.CAI_ANNEAL, "test" + ".pdf")
+    cfg.DATA.PROCESSED.CAI_ANNEAL, args.fig)
+p.save(fig_file)
